cc459c7e418503342fb055834fe52d6f0880fe4f
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-25 19:41:19 +0600

    Created new custom exception. Modified find() method, so it will throw custom exception in case the student is "null". In the main() method added a try-catch block to handle the custom exception.

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,15 +6,23 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    Student student = Student.getValueOf(studentID);
+    if (student == null) {
+      throw new StudentNotFoundException(studentID);
+    }
+    return student;
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      } catch (StudentNotFoundException e) {
+        System.out.println(e.getMessage());
+      }
     }
 
   }
